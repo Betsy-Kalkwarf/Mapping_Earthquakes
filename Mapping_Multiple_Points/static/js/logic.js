004b0c8f26a7f12732d4b945b31f9ec911adc0ad
Betsy Kalkwarf <109998791+Betsy-Kalkwarf@users.noreply.github.com>
2022-11-02 18:16:16 -0700

    Add files via upload
    more points on map

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,14 +2,24 @@
 console.log("working");
 
 // Create the map object with a center and zoom level (4 on scale 0-18)
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-L.circle([34.0522, -118.2437], {
-    radius: 3000,
-    color: "black",
-    fillOpacity: 0.25,
-    fillColor: "yellow"
-}).addTo(map);
+//data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, 
+        {radius: city.population/100000,
+        color: "orange",
+        fillColor: "orange"
+    
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+   });
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
