44597ea854160f881043e3113efae576e2674176
Betsy Kalkwarf <109998791+Betsy-Kalkwarf@users.noreply.github.com>
2022-11-02 18:33:44 -0700

    Add files via upload
    circles and lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,7 +2,24 @@
 console.log("working");
 
 // Create the map object with a center and zoom level (4 on scale 0-18)
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([37.6213, -100.3790], 5);
+
+// Coordinates for each point to be used in the line.
+let line = [
+    [37.6213, -122.3790],
+    [30.1975, -97.6664],
+    [41.9803, -87.9090],
+    [43.6777, -79.6248],
+    [40.6413, -73.7781]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "blue",
+    style: "dashed",
+    weight: 0.4,
+    opacity: 0.5
+  }).addTo(map);
 
 //data from cities.js
 let cityData = cities;
@@ -21,7 +38,7 @@ cityData.forEach(function(city) {
    });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
